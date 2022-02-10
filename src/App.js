e75efa0c161b06086621bb7eac7f204dda0933a1
Haoran Zhou <kens0serious@gmail.com>
2022-02-09 22:40:08 -0500

    drak mode toggle

--- src/App.js
+++ src/App.js
@@ -2,13 +2,34 @@ import React, { useState, useEffect } from 'react'
 import data from './data'
 import Article from './Article'
 
+const getStorageTheme = () => {
+  return localStorage.getItem('theme') || 'light-theme'
+}
+
 function App() {
+  const [theme, setTheme] = useState(getStorageTheme)
+
+  const toggleTheme = () => {
+    if (theme === 'light-theme') {
+      setTheme('dark-theme')
+    } else {
+      setTheme('light-theme')
+    }
+  }
+
+  useEffect(() => {
+    document.documentElement.className = theme
+    localStorage.setItem('theme', theme)
+  }, [theme])
+
   return (
     <main>
       <nav>
         <div className="nav-center">
           <h1>overreacted</h1>
-          <button className="btn">toggle</button>
+          <button className="btn" onClick={toggleTheme}>
+            toggle
+          </button>
         </div>
       </nav>
       <section className="articles">
